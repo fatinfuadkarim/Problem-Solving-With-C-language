1e9a83733df7d594dd21d651c465b3f7c2bbe2b7
Fatin Fuad Karim <56217090+fatinfuadkarim@users.noreply.github.com>
2022-04-26 23:08:27 +0600

    Day : #5

---
+++ Home work 5/6a.c
@@ -0,0 +1,19 @@
+#include <stdio,h>
+void main(){
+int i=16>>2, j=5<<3;
+int k;
+printf("i=%d, j=%d\n",i,j);
+i=i|16;
+printf("i=%d, j=%d\n",i,j);
+k=j|16;
+printf("i=%d, k=%d\n",i,k);
+if(j%2==0){
+j=j&16;
+printf("i=%d, j=%d\n",i,j);
+}
+if(i<j)
+printf("i=%d, j=%d\n",++i,j++);
+else
+printf("i=%d, j=%d\n",i++,++j);
+printf("Done")
+}
